24375d9122a7daa1294ea1807abf678b256947fa
Jan Šíma <honzasima@gmail.com>
2019-08-21 11:14:22 +0200

    update to new version Flutter, androidx & IOS

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(ContactsExampleApp());
